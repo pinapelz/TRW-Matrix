ebb27f1a33357aa4de46c96508f38d7b9bfedf72
Pinapelz <yukais@pinapelz.com>
2026-03-25 17:56:31 -0700

    add gyu

--- constants.py
+++ constants.py
@@ -1,5 +1,6 @@
 TRIGGERS = {
     "hello nori": "brooooo srsly. out of all the japanese food u pick the one where you have to sit on hard ass stools and pay $9999 to eat 1 handroll",
     "danbo": "HELLSS NAHHH WE ARE NOT EATING THAT CUH. its not halal. long ass lines mid ass ramen",
-    "saku": "lil brother. this is not only very disrespectfully all pork, we have gone there 1 billion times and the pricing is off the charts."
+    "saku": "lil brother. this is not only very disrespectfully all pork, we have gone there 1 billion times and the pricing is off the charts.",
+    "gyu": "gyukaku man? ong im not down to cook my own food and give them a juicy tip"
 }
